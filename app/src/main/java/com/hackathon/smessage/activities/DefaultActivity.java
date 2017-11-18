03288fc083b60d2f378af3de82ed120cdacdf3f3
truong dang duy trung <segawasai@gmail.com>
2017-11-18 15:20:58 +0700

    update new Icon, add No message screen, add navigation bar menu, add DefaultActivity, request Permission

---
+++ app/src/main/java/com/hackathon/smessage/activities/DefaultActivity.java
@@ -0,0 +1,71 @@
+package com.hackathon.smessage.activities;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hackathon.smessage.configs.Defines;
+import com.hackathon.smessage.controllers.ContactOpearation;
+import com.hackathon.smessage.sqlitehelper.SqliteHelper;
+
+
+public class DefaultActivity extends AppCompatActivity {
+
+    protected Activity mActivity;
+
+    //broadcast: update inbox
+    protected BroadcastReceiver mBroadcastReceivedSMS;
+    protected IntentFilter mIntentFilterSMS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mIntentFilterSMS = new IntentFilter(Defines.ACTION_RECEIVE_SMS);
+        if(!ContactOpearation.isInitialized()){
+            ContactOpearation.init(this);
+        }
+        if(!SqliteHelper.isInitialized()){
+            SqliteHelper.init(this);
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mBroadcastReceivedSMS != null){
+            registerReceiver(mBroadcastReceivedSMS, mIntentFilterSMS);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mBroadcastReceivedSMS != null){
+            unregisterReceiver(mBroadcastReceivedSMS);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
--- app/src/main/java/com/hackathon/smessage/activities/MainActivity.java
+++ app/src/main/java/com/hackathon/smessage/activities/MainActivity.java
@@ -1,17 +1,134 @@
 package com.hackathon.smessage.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.hackathon.smessage.BuildConfig;
 import com.hackathon.smessage.R;
+import com.hackathon.smessage.utils.PermissionUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends DefaultActivity {
 
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private NavigationView mNavigationView;
+    //use load in thread
+    private Handler mHandlerThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        firstInit();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(PermissionUtils.onRequestPermissionsResult(this, requestCode)){
+            init();
+        }
+    }
+    //-------------------------------------------
+    private void showSplash(){
+        setContentView(R.layout.splash_main);
+        TextView tvVersion = (TextView)findViewById(R.id.textViewVersion);
+        tvVersion.setText(getString(R.string.version) + " " + BuildConfig.VERSION_NAME);
+    }
+
+    private void firstInit(){
+        mActivity = this;
+        showSplash();
+
+        mHandlerThread = new Handler(){
+            @Override
+            public void handleMessage(android.os.Message msg) {
+                super.handleMessage(msg);
+                    start();
+            }
+        };
+        if(PermissionUtils.isAllPermissionAlready(this)) {
+            init();
+        }
+        else{
+            PermissionUtils.requestPermissions(this);
+        }
+    }
+    //load
+    private void init(){
+        PermissionUtils.requireRegisterDefaultApp(this);
+
+        android.os.Message msg = mHandlerThread.obtainMessage();
+        mHandlerThread.sendMessage(msg);
+    }
+
+    private void start(){
+        getWidgets();
+        setWidgets();
+        addWidgetsListener();
+    }
+
+    private void getWidgets(){
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+    }
 
+    private void setWidgets(){
+        setSupportActionBar(mToolbar);
+        mNavigationView.setItemIconTintList(null);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                                    R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mActionBarDrawerToggle.syncState();
+    }
 
+    private void addWidgetsListener(){
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                if(item.isChecked()){ //skip if re-selected
+                    mDrawerLayout.closeDrawer(Gravity.START);
+                    return false;
+                }
+
+                switch (item.getItemId()){
+                    case R.id.menu_inbox_common:
+
+                        break;
+                    case R.id.menu_inbox_security:
+
+                        break;
+                    case R.id.menu_blocked_call_sms:
+                        break;
+                    case R.id.menu_setting:
+
+                        break;
+                }
+                mDrawerLayout.closeDrawer(Gravity.START);
+                return false;
+            }
+        });
     }
 }
